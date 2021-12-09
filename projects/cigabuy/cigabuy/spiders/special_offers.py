9061cdf17c0efb28f7d27b755cdf12da769c34d2
kvsekhar <kvsekhar@valuechain.com>
2021-12-09 19:40:07 +0530

    compant.csv to company.csv, changing header to mimic the requests sent by browser

--- projects/cigabuy/cigabuy/spiders/special_offers.py
+++ projects/cigabuy/cigabuy/spiders/special_offers.py
@@ -4,23 +4,35 @@ import scrapy
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
     allowed_domains = ['www.cigabuy.com/specials.html']
-    start_urls = ['https://www.cigabuy.com/specials.html']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.cigabuy.com/specials.html', callback=self.parse, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'
+        })
 
     def parse(self, response):
         for product in response.xpath("//ul[@class='productlisting-ul']/div/div"):
             if product.xpath(".//div[@class='p_box_price cf']/text()").get() is None:
-                discounted_price = product.xpath(".//div[@class='p_box_price cf']/span[1]/text()").get()
-                original_price = product.xpath(".//div[@class='p_box_price cf']/span[2]/text()").get()
+                discounted_price = product.xpath(
+                    ".//div[@class='p_box_price cf']/span[1]/text()").get()
+                original_price = product.xpath(
+                    ".//div[@class='p_box_price cf']/span[2]/text()").get()
             else:
-                original_price = product.xpath(".//div[@class='p_box_price cf']/text()").get()
+                original_price = product.xpath(
+                    ".//div[@class='p_box_price cf']/text()").get()
                 discounted_price = original_price
             yield{
-                'title':product.xpath(".//a[@class='p_box_title']/text()").get(),
-                'url':product.xpath(".//a[@class='p_box_title']/@href").get(),
-                'discounted_price':discounted_price,
-                'original_price':original_price
+                'title': product.xpath(".//a[@class='p_box_title']/text()").get(),
+                'url': product.xpath(".//a[@class='p_box_title']/@href").get(),
+                'discounted_price': discounted_price,
+                'original_price': original_price,
+                'User-Agent':response.request.headers['User-Agent']
             }
+
         next_page = response.xpath("//a[@class='nextPage']/@href").getall()[0]
+
         if next_page:
-            print(next_page)
-            yield scrapy.Request(url=next_page, callback=self.parse, dont_filter=True)
+           # print(next_page)
+            yield scrapy.Request(url='https://www.cigabuy.com/specials.html', callback=self.parse,dont_filter=True ,headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'
+            })
